64fb45c7c2f46073ff7263197a973516eccc5d1e
zahidul76 <zahidul.islam20152015@gmail.com>
2022-06-11 15:41:32 -0500

    Initial Commit

---
+++ src/main/java/ArrayInJava/Vehicles.java
@@ -0,0 +1,18 @@
+package ArrayInJava;
+
+import java.net.StandardSocketOptions;
+
+public class Vehicles {
+    public static void main(String[] args) {
+        String [] cars = {"Tesla","Honda","Toyota","BMW"};
+        System.out.println (cars[0]);
+        System.out.println (cars[1]);
+        System.out.println (cars.length);
+
+
+
+
+
+
+    }
+}
---
+++ src/main/java/ArrayTwoDimentional.java
@@ -0,0 +1,7 @@
+public class ArrayTwoDimentional {
+    public static void main(String[] args) {
+        int [][] numbers = {{10,20,30,40},{5,15,25,35}};
+        int x = numbers [1][2];
+        System.out.println(x);
+    }
+}
